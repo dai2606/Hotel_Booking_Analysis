09969cb2a5ff9eead8599f06140cfb1147baaed0
Nguyễn Đại <nguyendai@BoyIT.local>
2026-06-04 14:57:14 +0900

    Add HDFS upload script

--- src/ingest/upload_to_hdfs.sh
+++ src/ingest/upload_to_hdfs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "Creating HDFS directory..."
 hadoop fs -mkdir -p /user/maria_dev/hotel_booking/raw
 
+echo "Uploading raw CSV files to HDFS..."
 hadoop fs -put -f data/raw/*.csv /user/maria_dev/hotel_booking/raw/
 
+echo "Checking uploaded files..."
 hadoop fs -ls /user/maria_dev/hotel_booking/raw
